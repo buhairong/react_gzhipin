bdfdba44be2f39c916d381667a871d24395ee209
buhairong <22858111@qq.com>
2019-03-23 22:44:49 +0800

    update

--- gzhipin-server/routes/index.js
+++ gzhipin-server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const md5 = require('blueimp-md5')
 const UserModel = require('../db/models').UserModel
-const filter = {password: 0}
+const filter = {password: 0, __v: 0} // 指定过滤属性
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -42,27 +42,37 @@ router.post('/register', function (req, res) {
     // 读取请求参数
     const {username, password, type} = req.body
     // 处理： 判断用户是否已经存在，如果存在，返回提示错误的信息，如果不存在，保存
-    // 判断用户是否已经存在
+    // 查询（根据username）
     UserModel.findOne({username}, function(err, user){
+        // 如果user有值(已存在)
         if (user) {
             res.send({code: 1, msg: '此用户已存在'})
-        } else {
+        } else { // 没值（不存在）
+            //  保存
             new UserModel({username, password: md5(password), type}).save(function(err, user){
+                // 生成一个cookie(userid: user.id), 并交给浏览器保存
+                res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7})
+
+                // 返回包含user的json数据
+                const data = {username, type, _id: user._id} // 响应数据中不要携带password
                 res.send({code: 0, data: user})
             })
         }
     })
-
-    // 返回响应数据
 })
 
 // 登录的路由
 router.post('/login', function (req, res) {
     const {username, password} = req.body
+    // 根据username和password查询数据库users, 如果没有，返回提示错误的信息，如果有，返回登陆成功信息（包含user）
     UserModel.findOne({username, password: md5(password)}, filter, function (err, user) {
-        if (!user) {
+        if (!user) { // 登陆失败
             res.send({code:1, msg: '用户名或密码错误'})
-        } else {
+        } else { // 登陆成功
+            // 生成一个cookie(userid: user.id), 并交给浏览器保存
+            res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7})
+
+            // 返回登陆成功信息（包含user）
             res.send({code:0, data: user})
         }
     })
