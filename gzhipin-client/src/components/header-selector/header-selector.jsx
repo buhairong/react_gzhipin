5dc7403d2eee0bd231791c20adb7b02cb039a1b6
buhairong <22858111@qq.com>
2019-03-25 23:16:55 +0800

    update

---
+++ gzhipin-client/src/components/header-selector/header-selector.jsx
@@ -0,0 +1,14 @@
+/*
+* 选中用户头像的UI组件
+* */
+import React, {Component} from 'react'
+
+export default class HeaderSelector extends Component {
+    render () {
+        return (
+            <div>
+                HeaderSelector
+            </div>
+        )
+    }
+}
---
+++ gzhipin-client/src/containers/dashen-info/dashen-info.jsx
@@ -0,0 +1,18 @@
+/*
+* 大神信息完善的路由容器组件
+* */
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class DashenInfo extends Component {
+    render () {
+        return (
+            <div>DashenInfo</div>
+        )
+    }
+}
+
+export default connect(
+    state => ({}),
+    {}
+)(DashenInfo)
