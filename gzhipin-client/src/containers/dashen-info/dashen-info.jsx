ac885f819d46c0820e9915b17042d976f17bed82
buhairong <22858111@qq.com>
2019-03-26 23:21:06 +0800

    add

--- gzhipin-client/src/containers/dashen-info/dashen-info.jsx
+++ gzhipin-client/src/containers/dashen-info/dashen-info.jsx
@@ -3,11 +3,44 @@
 * */
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {NavBar, InputItem, TextareaItem, Button} from 'antd-mobile'
+
+import HeaderSelector from '../../components/header-selector/header-selector'
 
 class DashenInfo extends Component {
+
+    state = {
+        header: '',
+        post: '',
+        info: ''
+    }
+
+    // 更新header状态
+    setHeader = (header) => {
+        this.setState({
+            header
+        })
+    }
+
+    handleChange = (name, value) => {
+        this.setState({
+            [name]: value
+        })
+    }
+
+    save = () => {
+
+    }
+
     render () {
         return (
-            <div>DashenInfo</div>
+            <div>
+                <NavBar>大神信息完善</NavBar>
+                <HeaderSelector  setHeader={this.setHeader} />
+                <InputItem placeholder="请输入求职岗位" onChange={val => {this.handleChange('post', val)}}>求职岗位：</InputItem>
+                <TextareaItem title="个人介绍" rows={3}  onChange={val => {this.handleChange('info', val)}} />
+                <Button type="primary" onClick={this.save}>保&nbps;&nbps;&nbps;存</Button>
+            </div>
         )
     }
 }
